822b4f46bb9fdefedd022358ff295ae8521aa5fe
nelson-wpwang <wpwang@bu.edu>
2016-12-12 16:01:01 -0500

    Update ProfileActivity.java

--- AndroidRTC-android-studio/app/src/main/java/fr/pchab/androidrtc/ProfileActivity.java
+++ AndroidRTC-android-studio/app/src/main/java/fr/pchab/androidrtc/ProfileActivity.java
@@ -162,3 +162,143 @@ public class ProfileActivity extends AppCompatActivity implements ChangePassword
     }
 }
 
+// public class ProfileActivity extends AppCompatActivity implements ChangePasswordDialog.Listener {
+
+//     public static final String TAG = ProfileActivity.class.getSimpleName();
+
+//     private TextView mTvName;
+//     private TextView mTvEmail;
+//     private TextView mTvDate;
+//     private Button mBtChangePassword;
+//     private Button mBtLogout;
+// //    private Button mBtAssistance;
+
+//     private ProgressBar mProgressbar;
+
+//     private SharedPreferences mSharedPreferences;
+//     private String mToken;
+//     private String mEmail;
+
+//     private CompositeSubscription mSubscriptions;
+
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_profile);
+//         mSubscriptions = new CompositeSubscription();
+//         initViews();
+//         initSharedPreferences();
+//         loadProfile();
+//     }
+
+//     private void initViews() {
+
+//         mTvName = (TextView) findViewById(R.id.tv_name);
+//         mTvEmail = (TextView) findViewById(R.id.tv_email);
+//         mTvDate = (TextView) findViewById(R.id.tv_date);
+//         mBtChangePassword = (Button) findViewById(R.id.btn_change_password);
+//         mBtLogout = (Button) findViewById(R.id.btn_logout);
+// //        mBtAssistance = (Button) findViewById(R.id.btn_Assistance);
+//         mProgressbar = (ProgressBar) findViewById(R.id.progress);
+
+// //        mBtAssistance.setOnClickListener(view -> jmprtc());
+//         mBtChangePassword.setOnClickListener(view -> showDialog());
+//         mBtLogout.setOnClickListener(view -> logout());
+//     }
+
+//     private void initSharedPreferences() {
+
+//         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//         mToken = mSharedPreferences.getString(Constants.TOKEN,"");
+//         mEmail = mSharedPreferences.getString(Constants.EMAIL,"");
+//     }
+
+//     private void logout() {
+
+//         SharedPreferences.Editor editor = mSharedPreferences.edit();
+//         editor.putString(Constants.EMAIL,"");
+//         editor.putString(Constants.TOKEN,"");
+//         editor.apply();
+//         finish();
+//     }
+
+// //    private void jmprtc() {
+// //        Intent intent = new Intent(this, RtcActivity.class);
+// //        startActivity(intent);
+// //        SharedPreferences.Editor editor = mSharedPreferences.edit();
+// //        editor.putString(Constants.EMAIL,"");
+// //        editor.putString(Constants.TOKEN,"");
+// //        editor.apply();
+// //        finish();
+// //    }
+
+//     private void showDialog(){
+
+//         ChangePasswordDialog fragment = new ChangePasswordDialog();
+
+//         Bundle bundle = new Bundle();
+//         bundle.putString(Constants.EMAIL, mEmail);
+//         bundle.putString(Constants.TOKEN,mToken);
+//         fragment.setArguments(bundle);
+
+//         fragment.show(getFragmentManager(), ChangePasswordDialog.TAG);
+//     }
+
+//     private void loadProfile() {
+
+//         mSubscriptions.add(NetworkUtil.getRetrofit(mToken).getProfile(mEmail)
+//                 .observeOn(AndroidSchedulers.mainThread())
+//                 .subscribeOn(Schedulers.io())
+//                 .subscribe(this::handleResponse,this::handleError));
+//     }
+
+//     private void handleResponse(User user) {
+
+//         mProgressbar.setVisibility(View.GONE);
+//         mTvName.setText(user.getName());
+//         mTvEmail.setText(user.getEmail());
+//         mTvDate.setText(user.getCreated_at());
+//     }
+
+//     private void handleError(Throwable error) {
+
+//         mProgressbar.setVisibility(View.GONE);
+
+//         if (error instanceof HttpException) {
+
+//             Gson gson = new GsonBuilder().create();
+
+//             try {
+
+//                 String errorBody = ((HttpException) error).response().errorBody().string();
+//                 Response response = gson.fromJson(errorBody,Response.class);
+//                 showSnackBarMessage(response.getMessage());
+
+//             } catch (IOException e) {
+//                 e.printStackTrace();
+//             }
+//         } else {
+
+//             showSnackBarMessage("Network Error2 !");
+//         }
+//     }
+
+//     private void showSnackBarMessage(String message) {
+
+//         Snackbar.make(findViewById(R.id.activity_profile),message,Snackbar.LENGTH_SHORT).show();
+
+//     }
+
+//     @Override
+//     protected void onDestroy() {
+//         super.onDestroy();
+//         mSubscriptions.unsubscribe();
+//     }
+
+//     @Override
+//     public void onPasswordChanged() {
+
+//         showSnackBarMessage("Password Changed Successfully !");
+//     }
+// }
+
